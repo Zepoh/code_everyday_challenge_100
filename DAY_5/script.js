cffb8a9d7df125e8d6aaca863d51f6177b004f96
zepoh <menzepohyvesseraphin@gmail.com>
2024-06-10 07:13:01 +0100

    first version of Day_5

---
+++ DAY_5/script.js
@@ -0,0 +1,12 @@
+const number_loading = document.querySelector("h1"); // thes
+
+
+function countNumber() {
+    
+}
+
+window.onload = function() {
+    console.log(number_loading);
+    setInterval(afficherHeure, 1000);
+};
+  
