01ba39fd562f3d73e2e7e29d22a7f9a3c51ad3e3
zepoh <menzepohyvesseraphin@gmail.com>
2024-06-10 17:06:01 +0100

    first version for day 2

---
+++ DAY_2_Progress_Steps/README.md
@@ -0,0 +1,2 @@
+# DAY_2 Progress Steps
+
--- DAY_5/script.js
+++ DAY_5/script.js
@@ -1,12 +1,29 @@
-const number_loading = document.querySelector("h1"); // thes
+const loader =  document.querySelector(".compteur"); //represents the text whose opacity decreases 
+const number_loading = document.querySelector(".compteur em"); // stand for the number whose the value increase
+const background_image = document.querySelector("img"); // represent the image whose the image blur decrease
 
+let step = 100; // cause we count from 0 to 100
 
-function countNumber() {
-    
-}
+loader.style.opacity = `${step}%`
+background_image.style.filter = `blur(${step}px)`
+
 
 window.onload = function() {
-    console.log(number_loading);
-    setInterval(afficherHeure, 1000);
+
+    function countNumber() {
+        number_loading.innerHTML = parseInt(number_loading.innerHTML) +1
+        step = step - 1;
+    
+        loader.style.opacity = `${step}%`
+        background_image.style.filter = `blur(${step}px)`
+    
+        if (step == 0) {
+            clearInterval(interval_identifier);
+        }
+        console.log(step);
+    }
+
+    console.log("au debut loader.style.opacity :", loader.style.opacity );
+    const interval_identifier = setInterval(countNumber, 50);
 };
   
