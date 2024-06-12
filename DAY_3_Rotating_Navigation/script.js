d7042291ed16f2f3907889ddadd4bdf5db720201
Zepoh-notquerty <menzepohyvesseraphin@gmail.com>
2024-06-12 06:58:13 +0100

    final version release v1 day 3

--- DAY_3_Rotating_Navigation/script.js
+++ DAY_3_Rotating_Navigation/script.js
@@ -1,23 +1,22 @@
 const body_content = document.querySelector('.body_content')
 const menu = document.querySelector('.menu')
-
+const menu_option =document.querySelector(".menu_option")
 
 let menu_is_open = false;
-
-
-console.log(body_content);
 
 
 function toogle_menu() {
     menu_is_open = !menu_is_open
     if (menu_is_open) {
         console.log("menu_is_open :", menu_is_open);
-        menu.classList.add("menu_rotate")
         body_content.style.rotate = "-20deg";
+        menu.classList.add("menu_rotate")
+        menu_option.classList.add("menu_option_opened")
     }else{
         console.log("menu_is_open :", menu_is_open);
-        menu.classList.remove("menu_rotate")
         body_content.style.rotate = "0deg";
+        menu.classList.remove("menu_rotate")
+        menu_option.classList.remove("menu_option_opened")
     }
 
 }
