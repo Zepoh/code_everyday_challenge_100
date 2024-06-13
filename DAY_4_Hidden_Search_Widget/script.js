37efaeb33dc2d2b2a25bf634af45db499c0c2315
zepoh <menzepohyvesseraphin@gmail.com>
2024-06-13 05:33:36 +0100

    TUTORIAL_DAY4: using classlist.toogle instead of if else. Using focus method

--- DAY_4_Hidden_Search_Widget/script.js
+++ DAY_4_Hidden_Search_Widget/script.js
@@ -1,16 +1,7 @@
-const body_content = document.querySelector(".body_content") 
 const input = document.querySelector(".body_content input")
 
-let is_expand = false
-
-console.log(input);
-
 function expand_input() {
-    console.log("click");
-    if (is_expand) {
-        input.classList.remove("input_expanded")
-    } else {
-        input.classList.add("input_expanded")
-    }
-    is_expand = !is_expand
+    input.classList.toggle("input_expanded")
+    console.log(input.classList);
+    input.focus();
 }
