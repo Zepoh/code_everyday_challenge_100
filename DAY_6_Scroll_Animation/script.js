815d0823d0fcdf8dbc4006d926b0fe2ccf589024
zepoh <menzepohyvesseraphin@gmail.com>
2024-06-13 16:38:23 +0100

    RELEASE_V1_DAY6

---
+++ DAY_6_Scroll_Animation/script.js
@@ -0,0 +1,26 @@
+const card = document.querySelectorAll(".content_wrapper div")
+
+// initialize 
+manager_card()
+
+window.addEventListener('scroll',()=>{
+    manager_card()
+})
+
+function manager_card() {
+    for (let i = 0; i < card.length; i++) {
+        const element = card[i];
+        if ((element.getBoundingClientRect().bottom) < window.innerHeight ) {
+            element.style.transform =  "translateX(0vw)"
+        } else {
+            if (i%2 == 0) {
+                element.style.transform =  "translateX(100vw)"
+            } else {
+                element.style.transform =  "translateX(-100vw)"
+            }
+        }
+    }
+}
+
+
+
