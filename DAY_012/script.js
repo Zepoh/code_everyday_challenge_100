9cf74971f623424f4906a287a392ea2ea5bbf0b9
zepoh <menzepohyvesseraphin@gmail.com>
2024-06-20 10:47:21 +0100

    major update for day 12 release v1

--- DAY_012/script.js
+++ DAY_012/script.js
@@ -1,36 +1,36 @@
-const question_s = document.querySelectorAll(".question")
+const question_s = document.querySelectorAll(".question");
 
-const path_image_cross = "M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" 
-const path_image_arrow = "M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
+const path_image_cross =
+  "M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z";
+const path_image_arrow =
+  "M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z";
 
-
-question_s.forEach(question => {
-    question.addEventListener("click", ()=>{
-        disable_question()
-        enable_question(question)
-    })
+question_s.forEach((question) => {
+  question.addEventListener("click", () => {
+    if (question.classList.contains("active")) {
+        disable_question(question);
+    } else {
+        disable_all_question();
+        enable_question(question);
+    }
+  });
 });
 
-
-
 function enable_question(question) {
-    const svg = question.children[2].children[0].children[0]
-
-    if (question.classList.contains("active")) {
-        console.log("faut retirer");
-        question.classList.remove("active")
-        svg.setAttribute("d",path_image_arrow)
-    } else {
-        question.classList.add("active")
-        svg.setAttribute("d",path_image_cross)
-    }
-    
+    const svg = question.children[2].children[0].children[0];
+    question.classList.add("active");
+    svg.setAttribute("d", path_image_cross);
 }
-
-function disable_question() {
+function disable_question(question) {
+    const svg = question.children[2].children[0].children[0];
+    svg.setAttribute("d", path_image_arrow);
+    question.classList.remove("active");
+}
+function disable_all_question() {
     question_s.forEach(question => {
-        const svg = question.children[2].children[0].children[0]
-        svg.setAttribute("d",path_image_arrow)
-        question.classList.remove("active")
+        const svg = question.children[2].children[0].children[0];
+        svg.setAttribute("d", path_image_arrow);
+        question.classList.remove("active");
     });
 }
+
