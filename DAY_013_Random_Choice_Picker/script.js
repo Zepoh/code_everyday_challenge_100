9897b7a3c9339f5de53d389630a51fa4c60e7677
zepoh <menzepohyvesseraphin@gmail.com>
2024-06-21 19:59:10 +0100

    major update day13

--- DAY_013_Random_Choice_Picker/script.js
+++ DAY_013_Random_Choice_Picker/script.js
@@ -1,63 +1,69 @@
-const text_area = document.querySelector("textarea")
-const choice_wrapper = document.querySelector(".choice_wrapper")
-const choice_innerHTML = `<div class="choice"><p>2</p></div>`
+const text_area = document.querySelector("textarea");
+const choice_wrapper = document.querySelector(".choice_wrapper");
+const choice_innerHTML = `<div class="choice"><p>2</p></div>`;
 
-let texte = ""
-let table_choice = []
-
+let texte = "";
+let table_choice = [];
+let clean_table_choice = [];
 
 text_area.value = ""
 
-
-text_area.addEventListener("keyup", (event)=>{
+text_area.addEventListener("keyup", (event) => {
   // console.log(e, "   ", e.key, "   ", typeof(e.key), "   ", e.key.length );
+  console.log(event.key);
 
-  remove_bad_choices()
+   remove_bad_choices()
 
-  create_choice() 
+  create_choice();
 
   if (event.key == "Enter") {
-    random_choice()
+    random_choice();
   }
-})
-
-
+});
 
 function create_choice() {
   // create choices
-  texte  = text_area.value
+  texte = text_area.value;
+  clean_table_choice = []
 
 
-  table_choice = texte.split(",")
-  console.log("derniere lettre du texte :", texte[texte.length - 1]);
+  table_choice = texte.split(",");
+  table_choice.forEach((element, index) => {
+    if (!(element == "" || element == " ")) {
+      
+      clean_table_choice.push(table_choice[index])  
+    }
+  });
+  console.log(clean_table_choice);
+ 
+
+
+
   console.log("table_choice :", table_choice);
 
-  // display choices 
-  choice_wrapper.innerHTML = ""
-  table_choice.forEach(element => {
-
+  // display choices
+  choice_wrapper.innerHTML = "";
+  clean_table_choice.forEach((element) => {
     if (element != "") {
-      let div = document.createElement("div")
-      div.innerHTML = `<div class="choice"><p>${element}</p></div>`
-      choice_wrapper.appendChild(div)
+      let div = document.createElement("div");
+      div.innerHTML = `<div class="choice"><p>${element}</p></div>`;
+      choice_wrapper.appendChild(div);
     }
   });
 }
 
-function remove_bad_choices() { //en gros on enleve les espaces qui se suivent
+function remove_bad_choices() {
+  //en gros on enleve les espaces qui se suivent
   //remplacer double espace par un seul. la lettre g c'est pour remplacer toutes les occurences
-  texte = text_area.value.replace(/  /g, " ") 
-  // texte = text_area.value.replace(/,  ,/g, ",") 
-  // texte = text_area.value.replace(/,,/g, ",") 
+   texte = text_area.value.replace(/  /g, " ")
+  // texte = text_area.value.replace(/\,  \,/g, ",")
+  // texte = text_area.value.replace(/,,/g, ",")
 
   text_area.value = texte
-
 }
 
-
 function random_choice() {
-  if ( choice_wrapper.innerHTML != "") {
+  if (choice_wrapper.innerHTML != "") {
     console.log("debut de random choice");
-    
   }
 }
