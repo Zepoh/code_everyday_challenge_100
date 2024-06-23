bd17a9267371499644ad52cf2835172054e32a2f
zepoh <menzepohyvesseraphin@gmail.com>
2024-06-23 09:35:40 +0100

    major update day14

---
+++ DAY_014_Animated_Navigation/script.js
@@ -0,0 +1,8 @@
+const icon_navigation  = document.querySelector(".nav_image")
+const link_wrapper = document.querySelector(".link_wrapper")
+
+
+function toggle_navbar() {
+    link_wrapper.classList.toggle("active")
+
+}
