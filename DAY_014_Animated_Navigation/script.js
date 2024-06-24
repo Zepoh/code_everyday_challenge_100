23a15c7fb895ce366dd1ec9c62853787b3184f5d
zepoh <menzepohyvesseraphin@gmail.com>
2024-06-24 03:23:52 +0100

    Update day 14 release v1

--- DAY_014_Animated_Navigation/script.js
+++ DAY_014_Animated_Navigation/script.js
@@ -1,5 +1,5 @@
 const icon_navigation  = document.querySelector(".nav_image")
-const link_wrapper = document.querySelector(".link_wrapper")
+const link_wrapper = document.querySelector(".nav_content")
 
 
 function toggle_navbar() {
