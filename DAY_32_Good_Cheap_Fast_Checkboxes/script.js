04e4e15ca9d5aba75213702c62c93e842fa37f8b
zepoh <menzepohyvesseraphin@gmail.com>
2024-08-26 18:35:32 +0100

    released day v1 32

---
+++ DAY_32_Good_Cheap_Fast_Checkboxes/script.js
@@ -0,0 +1,22 @@
+let criteria_S = document.querySelectorAll(".criteria");
+
+for (let index = 0; index < criteria_S.length; index++) {
+  const criteria = criteria_S[index];
+  criteria.addEventListener("click", () => {
+    criteria.classList.toggle("active");
+    console.log(criteria.children[0].children[0]);
+    let i = 0;
+    criteria_S.forEach((element) => {
+      if (element.classList.contains("active")) {
+        i++;
+      }
+    });
+    if (i == 3) {
+      if (index == criteria_S.length - 1) {
+        criteria_S[0].classList.remove("active");
+      } else {
+        criteria_S[index + 1].classList.remove("active");
+      }
+    }
+  });
+}
